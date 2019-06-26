31369bc9e1f5ae328d5c07a7f1b07e0a7846f521
zhangyuncai <zhangyc@asia5b.com>
2019-06-26 14:03:59 +0800

    新建ToastUtil工具类

--- app/src/main/java/com/zhang/jitdemo/MainActivity.kt
+++ app/src/main/java/com/zhang/jitdemo/MainActivity.kt
@@ -2,11 +2,13 @@ package com.zhang.jitdemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.zhang.libutis.ToastUtil
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ToastUtil.toastContext = applicationContext
     }
 }
--- libutis/src/main/java/com/zhang/libutis/ToastUtil.kt
+++ libutis/src/main/java/com/zhang/libutis/ToastUtil.kt
@@ -1,7 +1,16 @@
 package com.zhang.libutis
 
+import android.content.Context
+import android.widget.Toast
+
 /**
  * Created by zhangyuncai on 2019/6/26.
  */
 class ToastUtil {
+    companion object {
+        lateinit var toastContext: Context
+        fun toastMsg(msg: String) {
+            Toast.makeText(toastContext, msg, Toast.LENGTH_SHORT).show()
+        }
+    }
 }
